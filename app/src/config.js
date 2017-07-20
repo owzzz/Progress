585403fba9ec8e8f311a28ed647c5c110c16273f
Owain Llewellyn <owain.llew@gmail.com>
2017-07-20 16:24:10 -0400

    Added Progress Class and functionality for pulling commits and templating them

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,34 @@
-module.exports.Config = {
+module.exports = {
+	default: {
+		mailOptions: {
+			from: "owain@selostudios.com",
+			to: "owain@selostudios.com",
+			subject: "Digest Report",
+			template: 'daily-digest'
+		},
+		aws: {
+			host: 'email-smtp.us-east-1.amazonaws.com',
+			port: 465,
+			secure: true,
+			auth: {
+				user: 'AKIAI62DORRPAONTXLAQ',
+				pass: 'An5HLnpc2ICrQoVx7CyE6yVvqb0gOciWXWuRXrN+LP8q'
+			}
+		}
+
+	},
 	repo: {
-		url: "https://github.com/owzzz/Progress/"
+		url: "https://api.github.com/repos/owzzz/Progress/commits"
+	},
+	templating: {
+		viewEngine: {
+        	extname: '.hbs',
+        	layoutsDir: 'templates/layouts/',
+        	defaultLayout : 'main',
+        	partialsDir : 'templates/partials/'
+    	},
+		viewPath: 'templates/views',
+		extName: '.hbs'
 	}
 }
+
--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,76 @@
 let Config = require('./config');
+let fetch = require('node-fetch');
+let nodemailer = require('nodemailer');
+let smtpTransport = require('nodemailer-smtp-transport');
+let hbsMailer = require('nodemailer-express-handlebars');
+let CronJob = require('cron').CronJob;
 
-console.log(Config);
+class Progress {
+	constructor(url, recipient = 'owain.llew@gmail.com') {
+		this.repoUrl = url;
+		this.recipient = recipient;
+
+		// Fetch Commits From Repository
+		this.constructor.getCommits(this.repoUrl)
+			.then(res => { return res.json() })
+			.then(json => this._handleCommits(json));
+	}
+
+	_handleCommits(commits) {
+		this.commits = commits;
+		console.log(commits);
+		if(this.commits.length > 0) {
+			this._initialiseTransporter();
+			// Send email via Transporter
+
+			Config.default.mailOptions.context = {
+				commits: commits
+			}
+			this._sendMail(Config.default.mailOptions);
+		} else {
+			console.log('no commits to report');
+		}
+	}
+
+	_initialiseTransporter() {
+		// Create Transporter and use handlebars templating engine
+		this.transporter = this._createTransportMethod(Config.default.aws);
+		this.transporter.use('compile', hbsMailer(Config.templating));
+
+		return this.transporter;
+	}
+
+	_createTransportMethod(options) {
+		return nodemailer.createTransport(options);
+	}
+
+	_sendMail(options) {
+		// send mail with defined transport object
+		this.transporter.sendMail(options, (error, info) => {
+		    if (error) {
+		        return console.log(error);
+		    }
+		    console.log('Message %s sent: %s', info.messageId, info.response);
+		    this.transporter.close();
+		});
+	}
+	static getCommits(url) {
+		try {
+			return fetch(Config.repo.url);
+		} catch (err) {
+			console.log(err);
+			return err;
+		}
+	}
+}
+
+let job = new CronJob({
+  cronTime: '*/5 * * * *',
+  onTick: function() {
+	let progress = new Progress({url: Config.repo, recipent: 'owain.llew@gmail.com'});
+  },
+  start: true,
+  timeZone: 'America/New_York'
+});
+
+job.start();
