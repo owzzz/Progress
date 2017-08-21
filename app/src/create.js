364a676302bb5e5a2493443e4b48d964ccf0629b
System Administrator <root@Owains-MBP.home>
2017-08-21 00:05:04 -0400

    Added Serverless files, incomplete

---
+++ app/src/create.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.create = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      ProjectID: uuid.v1(),
+      EmailSubscriber: data.emailSubscriber,
+      RepositoryUrl: data.repositoryUrl,
+      Frequency: data.frequency,
+      CreatedAt: timestamp,
+      UpdatedAt: timestamp,
+    },
+  };
+
+  console.log(params);
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(new Error('Couldn\'t create the project.'));
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+};
